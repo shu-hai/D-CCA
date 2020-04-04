b673158160a3a08adced46546a75c67b2c10ffaa
Hai Shu <shuhai.edu@gmail.com>
2020-04-04 01:00:51 -0400

    Update example.py

--- example.py
+++ example.py
@@ -16,8 +16,11 @@ Y_2 = np.loadtxt('Y_2.txt')
 
 r_1, r_2, r_12 = 3, 5, 1 # r_k=rank{cov(x_k)}, r_12=rank{cov(x_1,x_2)}; Or see the definition of r_1, r_2 and r_12 in the JASA paper
 
-#Use the true r_1, r_2 and r_12
+#Use user-specified r_1, r_2 and r_12
 X_1_hat, X_2_hat, C_1_hat, C_2_hat, D_1_hat, D_2_hat, r_1_hat, r_2_hat, r_12_hat, ccor_hat, theta_hat = dcca.dCCA(Y_1, Y_2, r_1=r_1, r_2=r_2, r_12=r_12)
+
+#Use user-specified r_1 and r_2, and estimate r_12 by 'MDL-IC'
+X_1_hat, X_2_hat, C_1_hat, C_2_hat, D_1_hat, D_2_hat, r_1_hat, r_2_hat, r_12_hat, ccor_hat, theta_hat = dcca.dCCA(Y_1, Y_2, r_1=r_1, r_2=r_2)
 
 #Estimate r_1 and r_2 by 'ED', and r_12 by 'MDL-IC'
 X_1_hat, X_2_hat, C_1_hat, C_2_hat, D_1_hat, D_2_hat, r_1_hat, r_2_hat, r_12_hat, ccor_hat, theta_hat  = dcca.dCCA(Y_1, Y_2, method='ED')
